46cd93cb99962c99435858a14f3651be989c8113
Luke Switzer <10099969+lukeswitz@users.noreply.github.com>
2022-02-04 17:19:33 -0700

    diagonal animation

--- src/components/board/Row.svelte
+++ src/components/board/Row.svelte
@@ -27,7 +27,7 @@
 	class:complete={guesses > num}
 >
 	{#each Array(COLS) as _, i}
-		<Tile bind:this={tiles[i]} state={state[i]} value={value.charAt(i)} position={i} />
+		<Tile bind:this={tiles[i]} state={evaluation[i]} value={value.charAt(i)} position={i+num} />
 	{/each}
 </div>
 
