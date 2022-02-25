7108b8d1996bd18ebdb005943cfdf4608ba3bbc5
Luke Switzer <10099969+lukeswitz@users.noreply.github.com>
2022-02-24 18:22:27 -0700

    increment version :hankey: this way for now

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-//! IF ANYTHING IN THIS FILE IS CHANGED MAKE SURE setVersion.js HAS ALSO BEEN UPDATED
+//! IF ANYTHING IN THIS FILE IS CHANGED MAKE SURE setVersion.js HAS ALSO BEEN UPDATED //TODO streamline version number
 import App from "./App.svelte";
 
 export default new App({
 	target: document.body,
 	props: {
-		version: "1.2.3",
+		version: "1.3.0",
 	}
 });
